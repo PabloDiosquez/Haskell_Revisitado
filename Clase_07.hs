01948a4b3a32cf4a12826f045161f3814ab676a0
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-06-06 18:20:04 -0300

    Actualización Clase 7

--- Clase_07.hs
+++ Clase_07.hs
@@ -20,5 +20,27 @@ incluido :: Eq a => Set a -> Set a -> Bool
 incluido [] _      = True 
 incluido (x:xs) cs = (pertenece x cs) && incluido xs cs 
 
-iguales :: Eq a => Set a -> Set b -> Bool 
-iguales xs cs = (incluido xs cs) && (incluido cs xs) 
+
+iguales :: Eq a => Set a -> Set a -> Bool 
+iguales xs cs = (incluido xs cs) && (incluido cs xs)
+
+
+union :: Eq a => Set a -> Set a -> Set a 
+union [] cs     = cs 
+union (x:xs) cs = union xs (agregar x cs) 
+
+
+interseccion :: Eq a => Set a -> Set a -> Set a
+interseccion [] _ = [] 
+interseccion (x:xs) cs | pertenece x cs = x : interseccion xs cs 
+					   | otherwise      =     interseccion xs cs 
+
+
+diferencia :: Eq a => Set a -> Set a -> Set a 
+diferencia [] _ = []
+diferencia (x:xs) cs | not (pertenece x cs) = x : diferencia xs cs 
+					 | otherwise            =     diferencia xs cs 
+
+
+diferenciaSimetrica :: Eq a => Set a -> Set a -> Set a 
+diferenciaSimetrica xs cs = diferencia (union xs cs) (interseccion xs cs) 
