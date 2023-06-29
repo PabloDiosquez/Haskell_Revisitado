ecc05ac1ecb39ca5d8a48897d7bd6f4de44df1b4
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-06-29 19:14:05 -0300

    Actualización TP 2

--- MásPractica/TP_2.hs
+++ MásPractica/TP_2.hs
@@ -26,3 +26,42 @@ siguiente Norte = Este
 siguiente Este  = Sur 
 siguiente Sur   = Oeste
 siguiente Oeste = Norte 
+
+-- 2.
+data Persona = MKPersona Nombre Edad 
+
+type Nombre = String 
+type Edad   = Int
+
+-- Función constructora 👷🏼‍
+-- Propósito:
+-- ▪ Describe una persona con el nombre dado y edad = a 0.
+-- Precondiciones:
+-- ▪ El nombre dado no debe ser vacío.
+--
+nacer :: Nombre -> Persona
+nacer nombre = MKPersona nombre 0 
+
+-- Propósito:
+-- ▪ Describe el nombre de la persona dada.
+-- Precondiciones:
+-- ▪ No tiene (es una función total).
+--
+nombre :: Persona -> Nombre
+nombre (MKPersona nombre _)= nombre 
+
+-- Propósito:
+-- ▪ Describe la edad de la persona dada.
+-- Precondiciones:
+-- ▪ No tiene (es una función total).
+--
+edad :: Persona -> Edad
+edad (MKPersona _ edad) = edad 
+
+-- Propósito:
+-- ▪ Describe a la persona dada con su edad aumentada en 1.
+-- Precondiciones:
+-- ▪ No tiene (es una función total).
+--
+crecer :: Persona -> Persona
+crecer (MKPersona nombre edad) = MKPersona nombre (edad + 1)
