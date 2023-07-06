d68ce091e1f9afa5769f8b58713e547d39567268
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-07-06 20:05:48 -0300

    Actualización Clase 3

--- MásPractica/Clase_03.hs
+++ MásPractica/Clase_03.hs
@@ -27,3 +27,54 @@ nroAnimales (o:os) =
 esAnimal :: Origen -> Bool 
 esAnimal Animal = True 
 esAnimal _      = False 
+
+-- Tipos productos (registros)🐣 -- Los constructores pueden tener parámetros -- 
+
+-- Un ingrediente cuenta con una descripción, su origen y un valor energético por cada 100gr
+-- expresados en calorías.
+
+type Nombre = String -- renombre de tipos 
+
+-- Propósito:
+-- Modela un ingrediente.
+-- Inv. Rep.:
+-- * El nombre del ingrediente no debe ser vacío.
+-- * El valor energético debe ser un número >= 0.
+
+data Ingrediente = MKI Nombre Origen Int 
+	deriving Show
+
+bollo :: Ingrediente
+bollo = MKI "Masa de pizza" Vegetal 228
+
+tomate :: Ingrediente
+tomate = MKI "Tomate" Vegetal 22 
+
+queso :: Ingrediente
+queso = MKI "Queso muzzarella" Animal 245  
+
+-- Funciones observadoras (proyectoras) 👁‍🗨
+
+-- Propósito:
+-- Describe el valor del primer campo (descripción) del ingrediente dado.
+-- Precondiciones:
+-- No tiene (es una función total).
+-- 
+descripcionI :: Ingrediente -> Nombre 
+descripcionI (MKI nombre _ _) = nombre 
+
+-- Propósito:
+-- Describe el origen del ingrediente dado.
+-- Precondiciones:
+-- No tiene (es una función total).
+-- 
+origenI :: Ingrediente -> Origen 
+origenI (MKI _ origen _) = origen 
+
+-- Propósito:
+-- Describe el valor energético del ingrediente dado.
+-- Precondiciones:
+-- No tiene (es una función total).
+-- 
+valorEnergeticoI :: Ingrediente -> Int 
+valorEnergeticoI (MKI _ _ valorEnergetico) = valorEnergetico 
