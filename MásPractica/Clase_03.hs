19cc87295e4566e20849daaf82a70087155e500a
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-07-06 19:19:59 -0300

    Actualización Clase 3

--- MásPractica/Clase_03.hs
+++ MásPractica/Clase_03.hs
@@ -2,26 +2,27 @@
 
 -- Tipos enumerativos 
 
-data Origen = Animal | Vegetal | Mineral deriving Show
+data Origen = Animal | Vegetal | Mineral 
+			  deriving Show
 
 -- Definir la función nroAnimales :: [Origen] -> Int 
 
 -- Propósito:
--- 
+-- Describe la cantidad de orígenes Animal que hay en la lista de orígenes dada.
 -- Precondiciones:
--- 
+-- No tiene (es una función total).
 -- 
 nroAnimales :: [Origen] -> Int 
 nroAnimales [] = 0  
-nroAnimales (origen:origenes) = 
-	if esAnimal origen
-		then 1 + nroAnimales origenes
-		else nroAnimales origenes 
+nroAnimales (o:os) = 
+	if esAnimal o
+		then 1 + nroAnimales os
+		else     nroAnimales os  
 
 -- Propósito:
--- 
+-- Indica si el origen dado es Animal.
 -- Precondiciones:
--- 
+-- No tiene (es una función total).
 -- 
 esAnimal :: Origen -> Bool 
 esAnimal Animal = True 
