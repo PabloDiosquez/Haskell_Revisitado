1010abc2bd536c33741e1c1f5426c8985931286c
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-08-09 15:02:47 -0300

    Se agrega la implementación del TAD Gramola

--- MásPractica/Gramola.hs
+++ MásPractica/Gramola.hs
@@ -1,5 +1,7 @@
 module Gramola where 
 
+import Resumen 
+
 -- Interfaz del Tipo Abstracto Gramola 🤘
 
 -- Propósito   : Crea una nueva gramola.
@@ -37,7 +39,65 @@ votarG         :: Int -> Gramola -> Gramola
 --
 puntajeG   	   :: Gramola -> Int 
 
--- Propósito  : Describe el nombre de la canción más votada en la gramola dada.
+-- Propósito  : Describe el nombre de la canción más votada en la gramola dada. Si nunca fue votado, 
+-- describe 0.
 -- Complejidad: O(1)
 --
 temaMasVotadoG :: Gramola -> String 
+
+-------------------------------------------------------------------------------------------------
+-- Implementación del tipo abstracto de datos Gramola ☂ 
+
+data Gramola = G [(String, Resumen)]	-- canciones anteriores con sus votos (en el orden inverso).
+				 String 			    -- canción actual. 
+				 Resumen 				-- votos del tema actual. 
+				 [(String, Resumen)]	-- canciones posteriores con sus votos. 
+				 String 				-- tema más votado.
+				 Int 					-- cantidad de votos del tema más votado. 
+
+-- INVARIANTE de representación: 
+-- El nombre del tema más votado es alguno de los nombres de temas de la gramola y es el que 
+-- tiene mayor cantidad de votos.
+--
+-- El número que representa la cantidad de votos del tema más votado debe ser verdaderamente 
+-- la cantidad de votos que recibió el tema más votado de toda la gramola.
+-- 
+
+-- ▪ 
+nuevaG (cancion : canciones) = 
+	G [] cancion nuevoR (cancionesConResumenesVacios canciones) cancion 0
+
+cancionesConResumenesVacios :: [String] -> [(String, Resumen)]
+cancionesConResumenesVacios [] 					  = [] 
+cancionesConResumenesVacios (cancion : canciones) = 
+	(cancion, nuevoR) : cancionesConResumenesVacios canciones
+
+-- ▪ 
+anteriorG (G [] act r sigs tmv ptmv) 				 =
+		   G [] act r sigs tmv ptmv
+anteriorG (G ((ant, rant):ants) act r sigs tmv ptmv) = 
+		   G  ants ant rant ((act, r):sigs) tmv ptmv
+
+-- ▪
+siguienteG (G ants act r [] tmv ptmv) 			  = 
+			G ants act r [] tmv ptmv
+siguienteG (G ants act r ((s, rs):sigs) tmv ptmv) = 
+			G ((act, r) : ants) s rs sigs tmv ptmv   
+
+-- ▪
+cancionG (G _ c _) = c 
+
+-- ▪ 
+votarG puntaje (G ants act r sigs tmv ptmv) = 
+	if cantidadR r == ptmv
+		then G ants act (agregarR puntaje r) act (ptmv + 1)
+		else G ants act (agregarR puntaje r) sigs tmv ptmv
+
+-- ▪ 
+puntajeG (G ants act r sigs tmv ptmv) = 
+	if cantidadR r == 0 
+		then 0 
+		else promedioR r 
+
+-- ▪ 
+temaMasVotadoG (G _ _ _ tmv _) = tmv 
